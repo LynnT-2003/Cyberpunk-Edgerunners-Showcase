f73072bc02ca24620ebb48e239706367ddf9af4d
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-05-21 01:49:26 +0700

    Initial commit.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,134 @@
-import Image from "next/image";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import { MacbookScroll } from "@/components/ui/macbook-scroll";
+import { ContainerScroll } from "@/components/ui/container-scroll";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+import { TextRevealCard, TextRevealCardTitle, TextRevealCardDescription } from "@/components/ui/text-reveal-card";
+import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
+import { Grid } from '@mui/material';
+import { CardStack } from "@/components/ui/card-stack";
+import { Spotlight } from "@/components/ui/spotlight";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
+import { CanvasRevealEffectDemo3 } from "@/components/ui/canvas-reveal-effect-section";
+import { CanvasRevealEffectDemo } from "@/components/ui/canvas-reveal-effect-demo";
 
 export default function Home() {
+
+  const quotes = [
+    {
+      id: 1,
+      name: 'David Martinez',
+      designation: '',
+      content: 'No Kid From Santo Will Ever Be A Suit.',
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      designation: 'UX Designer',
+      content: 'I aint worth it Lucy. Besides you, I got nothing left. But you still have a dream to stick around for. I need you to see it through. Thats my dream. Honestly, nothing else ever really mattered.',
+    },
+    // Add more Card objects as needed
+  ];
+  
+  const dummyProducts = [
+    { title: "Product 1", link: "/product1", thumbnail: "/lucy-smoke-1.webp" },
+    { title: "Product 2", link: "/product2", thumbnail: "/lucy-rocket.gif" },
+    { title: "Product 3", link: "/product3", thumbnail: "/lucy-smoke-1.webp" },
+    { title: "Product 4", link: "/product4", thumbnail: "/lucy-light-up.gif" },
+    { title: "Product 5", link: "/product5", thumbnail: "/lucy-rocket.gif" },
+    { title: "Product 6", link: "/product6", thumbnail: "/lucy-hug.gif" },
+    { title: "Product 7", link: "/product7", thumbnail: "/lucy-jacket.gif" },
+    { title: "Product 8", link: "/product8", thumbnail: "/david-lucy.gif" },
+    { title: "Product 9", link: "/product9", thumbnail: "/lucy-david-ending.gif" },
+    { title: "Product 10", link: "/product10", thumbnail: "/david-doc.gif" },
+    { title: "Product 11", link: "/product11", thumbnail: "/lucy-david-ending.gif" },
+    { title: "Product 12", link: "/product12", thumbnail: "/lucy-moon-ending.gif" },
+    { title: "Product 13", link: "/product13", thumbnail: "/moon-main.gif" },
+    { title: "Product 14", link: "/product14", thumbnail: "/lucy-moon-smile.gif" },
+    { title: "Product 15", link: "/product15", thumbnail: "/the-end.gif" },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+    <HeroParallax products={dummyProducts}/>
+    
+    <CanvasRevealEffectDemo3 />
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <CanvasRevealEffectDemo />
+
+    <MacbookScroll
+      src="/lucy-edit.gif"
+      title="Lucy, the star of our show."
+    />
+
+    <HeroHighlight className="mt-0">
+      <ContainerScroll titleComponent={undefined}>
+        <img
+          src={"/moon.gif"}
+          alt="Cyberpunk Edgerunners"
+          className="h-full"
         />
+      </ContainerScroll>
+    </HeroHighlight>
+
+    <HeroHighlight>
+      <h1 className="text-4xl md:text-6xl font-bold text-black dark:text-white mt-10">
+        A happy ending? For folks like us? 
+      </h1>
+      <h1 className="text-4xl md:text-6xl font-bold text-black dark:text-slate-900 mt-5">
+        <Highlight className="text-white-500">Wrong city. Wrong people.</Highlight>
+      </h1>
+      <div className="flex items-center justify-center mb-20">
+        <TextRevealCard
+          text="I aint worth it Lucy."
+          revealText="All I wanted was for you to live."
+          className="w-[60rem] mt-20"
+        >
+          <TextRevealCardTitle className="text-3xl w-[80rem]">Welcome to Night City</TextRevealCardTitle>
+          <TextRevealCardDescription className="text-xl mt-6">
+            A modern multicultural metropolis, a promised city of dreams where no one gets what they deserve. Cyberpunk was never about saving the world, it’s about saving yourself.
+          </TextRevealCardDescription>
+          <TextRevealCardDescription className="text-xl mt-4">
+          &nbsp;- "Give up while you're ahead choom."
+          </TextRevealCardDescription>
+        </TextRevealCard>
       </div>
+    </HeroHighlight>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="mb-6 ml-48">
+      <InfiniteMovingCards 
+          items={[
+            {
+              quote: "I'll Take You To The Moon! I Promise!",
+              name: "David Martinez,",
+              title: "Arasaka Dropout",
+            },
+            {
+              quote: "Hold on to your wishes if you can’t hold on to me.",
+              name: "Lucy,",
+              title: "Netrunner",
+            },
+            {
+              quote: "My son at Arasaka Tower Top floor! I can just see it.",
+              name: "Gloria Martinez,",
+              title: "Emergency Medical Technician",
+            },
+            {
+              quote: "Just another tale for another dreamer.",
+              name: "Doc,",
+              title: "Ripper",
+            },
+            {
+              quote: "Sorry, wish we couldn't go to the moon together.",
+              name: "David Martinez,",
+              title: "Cyberpunk",
+            },
+          ]}
+          direction="left" // Optional: Specify the animation direction
+          speed="normal" // Optional: Specify the animation speed
+          pauseOnHover={true} // Optional: Pause animation on hover
+      />
+    </div>
+    </>
   );
 }
